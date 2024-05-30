265607a0808b77c0393f9a6efe52ca6fb46fd1d0
bremariano <bre.mariano0@gmail.com>
2024-05-30 18:55:53 -0300

    Refatorando Cadastros/Atualização de dados

--- sistema/Nucleo/Modelo.php
+++ sistema/Nucleo/Modelo.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace sistema\Nucleo;
-use PDOException;
+
 use sistema\Nucleo\Conexao;
 
 class Modelo
@@ -63,7 +63,46 @@ if ($todos){
 }
 return $stmt->fetchObject();
     } catch (\PDOException $ex){
-$this->erro = $ex;
+echo $this->erro = $ex;
+return null;
     }
+}
+protected function cadastrar(array $dados)
+{
+    try {
+        $colunas = implode(',', array_keys($dados));
+        $valores = ':'.implode(',:', array_keys($dados));
+        $query = "INSERT INTO ".$this->tabela." ($colunas) VALUES ({$valores})";
+        $stmt = Conexao::getInstancia()->prepare($query);
+        $stmt->execute($this->filtro($dados));
+        return Conexao::getInstancia()->lastInsertId();
+    }catch (\PDOException $ex){
+        echo $this->erro = $ex;
+        return null;
+    }
+}
+protected function atualizar(array $dados, string $termo)
+{
+    try {
+        $set =[];
+        foreach ($dados as $chave => $valor){
+            $set[] = "{$chave} = :{$valor}";
+        }
+        $set = implode(', ', $set);
+        $query = "UPDATE ".$this->tabela." SET {$set} WHERE {$termo}";
+        $stmt = Conexao::getInstancia()->prepare($query);
+        $stmt->execute($this->filtro($dados));
+        return ($stmt->rowCount() ?? 1);
+    }catch (\PDOException $ex){
+        echo $this->erro = $ex;
+        return null;
+    }
+}
+private function filtro(array $dados)
+{
+$filtro = [];
+foreach ($dados as $chave => $valor){
+    $filtro[$chave] = (is_null($valor) ? null : filter_var($valor, FILTER_DEFAULT));
+}
 }
 }
