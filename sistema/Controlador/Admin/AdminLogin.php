253d4c38613bd740a99bcaf306442f0eadf79a11
bremariano <bre.mariano0@gmail.com>
2024-06-02 21:35:06 -0300

    Validando campos de formulários/ ajuste de visualização

--- sistema/Controlador/Admin/AdminLogin.php
+++ sistema/Controlador/Admin/AdminLogin.php
@@ -18,6 +18,26 @@ class AdminLogin extends Controlador
     }
     public function login():void
     {
+        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if (isset($dados)){
+            if (in_array('', $dados)){
+                $this->mensagem->alerta('Todos os campos são obrigatórios!')->flash();
+            } else{
+                $this->mensagem->sucesso('Dados válidos!')->flash();
+            }
+        }
 echo $this->template->renderizar('login.html', array());
     }
+//    private function checarDados(array $dados):bool
+//    {
+//        if (empty($dados['email'])){
+//            $this->mensagem->alerta('O campo de e-mail é obrigatório!')->flash();
+//            return false;
+//        }
+//        if (empty($dados['senha'])){
+//            $this->mensagem->alerta('O campo de senha é obrigatório!')->flash();
+//            return false;
+//        }
+//        return true;
+//    }
 }
