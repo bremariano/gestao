3faded459347ab0504d6a7f8d0f9c2703d09fd05
bremariano <bre.mariano0@gmail.com>
2024-06-19 07:20:48 -0300

    introdução a biblioteca de uploads do verot

--- index.php
+++ index.php
@@ -2,22 +2,42 @@
 //Arquivo index responsável pela inicialização do sistema
 require 'vendor/autoload.php';
 
-//require 'rotas.php';
-use sistema\Biblioteca\Upload;
-
-$upload = new Upload('upload');
-
 if (!empty($arquivo = $_FILES)) {
-    $arquivo = $_FILES['arquivo'];
-    $upload->arquivo($arquivo, 'novo nome', 'imagens');
-    if($upload->getResultado()){
-        echo 'upload feito com sucesso'. $upload->getResultado();
-    }else {
-        echo $upload->getErro();
+    $handle = new \Verot\Upload\Upload($_FILES['arquivo']);
+    if ($handle->uploaded) {
+        $handle->file_new_name_body = 'image_resized';
+        $handle->image_resize = true;
+        $handle->image_x = 100;
+        $handle->image_ratio_y = true;
+        $handle->process('uploads/imagens/');
+        if ($handle->processed) {
+            echo 'upload feito com sucesso';
+            $handle->clean();
+        } else {
+            echo 'error : ' . $handle->error;
+        }
     }
-
-    r($upload);
 }
+
+
+
+
+//require 'rotas.php';
+//use sistema\Biblioteca\Upload;
+//
+//$upload = new Upload('upload');
+//
+//if (!empty($arquivo = $_FILES)) {
+//    $arquivo = $_FILES['arquivo'];
+//    $upload->arquivo($arquivo, 'novo nome', 'imagens');
+//    if($upload->getResultado()){
+//        echo 'upload feito com sucesso'. $upload->getResultado();
+//    }else {
+//        echo $upload->getErro();
+//    }
+//
+//    r($upload);
+//}
 
 echo '<hr>';
 ?>
